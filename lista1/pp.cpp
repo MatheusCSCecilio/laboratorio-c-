f5f49580d2731d7843d6fcc36bb1712df3ab39d7
matheus <matheuscaua.cecilio@gmail.com>
2024-03-29 17:09:18 -0300

    lista1

---
+++ lista1/pp.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+      int Numero1,Numero2,Soma,Diferenca,Multiplicacao,Potencia;
+         float Raiz,Divisao;
+            
+                     printf("Digite dois numeros");
+      scanf("Numero1 %d);
+
+            return 0;
+            
+      }
+      
+
